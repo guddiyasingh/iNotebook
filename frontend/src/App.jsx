1ebc770d3f2990759cabee9b793789a84af03d83
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-04 05:42:28 +0530

    add navbar file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import Home from "./pages/Home/Home"
+
+import { Route, BrowserRouter as Router,Routes,Routes } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+// import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home"
 
 
-function App() {
+const routes = (
+
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path ="/signup" element = {<SignUp/>}/>
+    </Routes>
+  </Router>
+
+)
+
+
+function App ()  {
+
  
+
+
   return (
     <>
-     <Home/>
-     
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   
     </>
   )
 }
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../../componenets/Navbar'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+  <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
     </div>
   )
 }

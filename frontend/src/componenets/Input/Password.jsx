9d3af8627e1cdfb66a708cd03e9a66909d7c7427
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-06 23:34:34 +0530

    add useState property and input property in password file

--- frontend/src/componenets/Input/Password.jsx
+++ frontend/src/componenets/Input/Password.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const Password = ({value, onChange, placeholder}) => {
+
+    const [isShoePassword,setIsShowPassword] = useState(false);
+ const toggleShowPassword = () => {
+
+    setIsShowPassword(! isShoePassword);
+ };
+
   return (
-    <div className='flex items-center bg-transparent'>
+   
+
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
       PasswordInput
+      <input
+      value={value}
+      onChanage={onChange}
+      type={isShoePassword ? "text" : "password"}
+      placeholder={placeholder || "password"}
+      className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none'
+      />
     </div>
   )
 }

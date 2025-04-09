f29d308d960e92c6f1c9b4ed03640e465438e6ed
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-09 20:58:32 +0530

    add searchBar file

--- frontend/src/componenets/Cards/ProfileInfo.jsx
+++ frontend/src/componenets/Cards/ProfileInfo.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { getInitials } from '../../utils/helper'
 
-const ProfileInfo = () => {
+const ProfileInfo = ({onLogout}) => {
   return (
-    <div className=''>
-      <div className=''>TU</div>
+    <div className='flex items-center gap-3'>
+      <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
+       {getInitials("Johan Willam")} 
+        </div>
+
+
       <div>
-        <p className='text-sm font-medium'>Willam</p>
-        <button className='' onClick={onLogout}>
+        <p className='text-sm font-medium'>John Willam</p>
+        <button className="text-sm text-slate-700 underline"
+        onClick={onLogout}>
           Logout
         </button>
       </div>
@@ -14,4 +20,4 @@ const ProfileInfo = () => {
   )
 }
 
-export default Profileinfo
+export default ProfileInfo

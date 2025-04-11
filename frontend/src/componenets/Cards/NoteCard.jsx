fdf07129126fa2d55b2e09d5021aac4d3a407ea7
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-11 13:26:03 +0530

    add some code in home page and NoteCard page

--- frontend/src/componenets/Cards/NoteCard.jsx
+++ frontend/src/componenets/Cards/NoteCard.jsx
@@ -10,26 +10,31 @@ const NoteCard = ({title,
     onDelete,
     onPinNote}) => {
   return (
-    <div className=''>
-        <div>
+    <div className='border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out'>
+        <div className='flex items-center justify-between mt-2'>
         <div>
             <h6 className='text-sm font-medium'>{title}</h6>
             <span className='text-xs text-slate-500'>{date}</span>
         </div>
 
-        <MdOutlinePushPin className=''onClick={onPinNote} />
-      
-    </div>
-    <p className=''>{content?.slice(0,60)}</p>
-    <div className=''>
+        <MdOutlinePushPin 
+        className={`icon-btn ${isPinned ? "text-primary" :"text-slate-300"}`}
+        onClick={onPinNote} />
+      </div>
+
+    <p className='text-xs text-slate-600 nt-2'>{content?.slice(0,60)}</p>
+
+    <div className='flex items-center'>
       <div className='text-xs text-slate-500'>{tags}</div>
+
       <div className='flex items-center gap-2'>
         <MdCreate
         className='icon-btn hover:text-green-600'
         onClick={onEdit}/>
 
         <MdDelete
-        className='icon-btn hover:text-red-500'/>
+        className='icon-btn hover:text-red-500'
+        onClick={onDelete}/>
       </div>
     </div>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,20 +9,43 @@ const Home = () => {
 
    <div className='container mx-auto'>
     
-
+<div className='grid grid-cols-3 gap-4 mt-8'>
     <NoteCard 
     title="Meeting on 7th April" 
     date="3rd Apr 2024" 
     content="Meeting on 7th April"
     tags= "#Meeting"
     isPinned ={true}
-    onEdit={()=>{} }
-    onDelete={()=>{}}
-    onPinNote={()=>{}}
+    onEdit={()=> {} }
+    onDelete={()=> {}}
+    onPinNote={()=> {}}
+    />
+
+<NoteCard 
+    title="Meeting on 7th April" 
+    date="3rd Apr 2024" 
+    content="Meeting on 7th April"
+    tags= "#Meeting"
+    isPinned ={true}
+    onEdit={()=> {} }
+    onDelete={()=> {}}
+    onPinNote={()=> {}}
+    />
+
+<NoteCard 
+    title="Meeting on 7th April" 
+    date="3rd Apr 2024" 
+    content="Meeting on 7th April"
+    tags= "#Meeting"
+    isPinned ={true}
+    onEdit={()=> {} }
+    onDelete={()=> {}}
+    onPinNote={()=> {}}
     />
    </div>
+   </div>
     </>
-  )
+  );
 }
 
 export default Home

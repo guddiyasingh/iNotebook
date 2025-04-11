299589f7f4ca4bb3ba830955cfe6c2f954481da5
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-11 15:00:14 +0530

    add button and MdAdd hooks

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../componenets/Navbar/Navbar'
 import NoteCard from '../../componenets/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md';
 
 const Home = () => {
   return (
@@ -42,8 +43,24 @@ const Home = () => {
     onDelete={()=> {}}
     onPinNote={()=> {}}
     />
+     <NoteCard 
+    title="Meeting on 7th April" 
+    date="3rd Apr 2024" 
+    content="Meeting on 7th April"
+    tags= "#Meeting"
+    isPinned ={true}
+    onEdit={()=> {} }
+    onDelete={()=> {}}
+    onPinNote={()=> {}}
+    />
    </div>
    </div>
+
+   <button className='w-16 h-16 flex items-center justify-center rounded-2xl
+   bg-blue-400 hover:bg-blue-600 absolute right-10 bottom-10 '>
+   
+    <MdAdd className="text-[32px] text-white" />
+   </button>
     </>
   );
 }

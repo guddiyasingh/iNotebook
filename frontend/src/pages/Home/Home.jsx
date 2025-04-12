e8bb9343a6e3f02c1a20b91021d1720fd4330ccd
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-12 18:42:58 +0530

    add some code

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,10 +4,10 @@ import NoteCard from '../../componenets/Cards/NoteCard'
 
 import AddEditNotes from './AddEditNotes';
 import { MdAdd } from 'react-icons/md';
-
+import {Modal} from 'react-modal';
 const Home = () => {
 
-  const [openAddEditModal, setOpenAddEditMode] = useState({
+  const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
     type: "add",
     data: null,
@@ -18,6 +18,7 @@ const Home = () => {
 
    <div className='container mx-auto '>
     <div className='grid grid-cols-3 gap-14 mt-8 ml-15 '>
+
     <NoteCard 
     title="Meeting on 7th April" 
     date="3rd Apr 2024" 
@@ -33,10 +34,27 @@ const Home = () => {
 
    <button className='w-16 h-16 flex items-center justify-center rounded-2xl
    bg-blue-400 hover:bg-blue-600 absolute right-10 bottom-10 mt-160 '>
-   
-    <MdAdd className="text-[32px] text-white" />
+   onClick={() =>{
+    setOpenAddEditModal({isShown: true, type: "add",data: null});
+   }}
+    <MdAdd 
+    className="text-[32px] text-white" />
    </button>
+   <Modal
+   isOpen={openAddEditModal.isShown}
+   onRequestClose={() => {}}
+   style={{
+     overlay: {
+       backgroundColor:"rgba(0,0,0,0.2)",
+     },
+   }}
+ 
+   contentLabel=""
+   className ="w-[40%] max-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll "
+   >
    <AddEditNotes/>
+   </Modal>
+  
     </>
   );
 }

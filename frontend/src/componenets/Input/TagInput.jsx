bf868ac581cc4d56aa28c5b981840c860d095255
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-13 21:03:59 +0530

    add MdAdd tag in TagInput

--- frontend/src/componenets/Input/TagInput.jsx
+++ frontend/src/componenets/Input/TagInput.jsx
@@ -5,6 +5,9 @@ const TagInput = () => {
     <div>
     <div className='flex items-center gap-4 nt-3'>
         <input type="text" className='' placeholder='Add tags'/>
+        <button className=''>
+            <MdAdd className="text-2xl text-blue-700 hover:text-white"/>
+        </button>
     </div>
     </div>
   )
